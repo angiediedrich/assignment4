3edf5a98488c7a99eb638856e5c475fec96b4155
angiediedrich <adiedrich@mcad.edu>
2017-09-24 20:50:19 -0500

    console.logs done
    Not sure if I should try to make more variables to account for the repeated text?

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ document.getElementById('BtnChurchill').addEventListener('click', function(){
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Ghandi" button.
-  console.log("This speech was written by [author name] in [year]. It is [is year B.C.E.] that this year is B.C.E.")
+  console.log("This speech was written by " + ghandi.author + " in " + ghandi.year + ". It is " 
+  + ghandi.bce + " that this year is B.C.E.")
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
-  console.log("This speech was written by [author name] in [year]. It is [is year B.C.E.] that this year is B.C.E.")
+  console.log("This speech was written by " + demosthenes.author + " in " + demosthenes.year + ". It is " 
+  + demosthenes.bce + " that this year is B.C.E.")
 });
 
 var churchill = {
