8616ff43e81f638a29aefe73f19d206c8de4d729
angiediedrich <adiedrich@mcad.edu>
2017-09-24 20:34:28 -0500

    working on console.logs

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
+  console.log("There are [x] speeches on the page.")
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Churchill" button.
+  console.log("This speech was written by [author name] in [year]. It is [is year B.C.E.] that this year is B.C.E.")
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Ghandi" button.
+  console.log("This speech was written by [author name] in [year]. It is [is year B.C.E.] that this year is B.C.E.")
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
+  console.log("This speech was written by [author name] in [year]. It is [is year B.C.E.] that this year is B.C.E.")
 });
 
 var churchill = {
